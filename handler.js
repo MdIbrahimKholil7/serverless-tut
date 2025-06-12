ef3061fec8d85a2c06e50ef1addc939174a2db13
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2025-06-12 15:31:50 +0600

    route changes for test build

--- handler.js
+++ handler.js
@@ -1,27 +1,4 @@
 const serverless = require("serverless-http");
-const index = require("./src/index");
 const { app } = require("./src/index");
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from root!",
-  });
-});
-
-app.get("/hello", (req, res, next) => {
-  return res.status(200).json({
-    message: "Hello from path!",
-  });
-});
-app.get("/test", (req, res, next) => {
-  return res.status(200).json({
-    message: "Test build route!",
-  });
-});
-
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: "Not Found",
-  });
-});
 
 exports.handler = serverless(app);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-//write test route
 const express = require("express");
 const app = express();
-app.get("/test", (req, res, next) => {
-    return res.status(200).json({
-        message: "Test build route!",
-    });
-});
-app.get("/users", (req, res, next) => {
-    return res.status(200).json({
-        message: "User retrieve!",
-        users: []
-    });
+
+// Define all routes BEFORE exporting app
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "Hello from root!" });
 });
 
+app.get("/hello", (req, res) => {
+    return res.status(200).json({ message: "Hello from path!" });
+});
+
+app.get("/test", (req, res) => {
+    return res.status(200).json({ message: "Test build route!" });
+});
+
+app.get("/users", (req, res) => {
+    return res.status(200).json({ message: "User retrieve!", users: [] });
+});
+
+app.use((req, res) => {
+    return res.status(404).json({ error: "Not Found" });
+});
 app.listen(3000, () => {
     console.log("App running on port.", 3000);
-})
-module.exports = app;
+});
+module.exports = { app };
